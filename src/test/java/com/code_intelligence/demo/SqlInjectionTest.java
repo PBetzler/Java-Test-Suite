58179c0085cc30b98af4e77a693fa8f7c53a6a22
Philip Betzler <philip-betzler@code-intelligence.com>
2023-12-12 18:54:46 +0100

    added value profile to cifuzz.yaml and changed sqlinjection tests

--- src/test/java/com/code_intelligence/demo/SqlInjectionTest.java
+++ src/test/java/com/code_intelligence/demo/SqlInjectionTest.java
@@ -10,23 +10,18 @@ import java.sql.SQLException;
 
 public class SqlInjectionTest {
 
-    private static SqlInjection project;
-
-
-    @BeforeAll
-    public static void fuzzerInitialize() throws SQLException {
-        // Initialize the project and connect to the database
-        project = new SqlInjection();
-        project.connect();
-    }
 
     @Test
-    public static void unitTest() throws SQLException {
+    public void unitTest() throws SQLException {
+        SqlInjection project = new SqlInjection();
+        project.connect();
         project.getUserByUsername("Dave");
     }
 
     @FuzzTest
-    public static void myFuzzTest(FuzzedDataProvider data) throws SQLException {
+    public void myFuzzTest(FuzzedDataProvider data) throws SQLException {
+        SqlInjection project = new SqlInjection();
+        project.connect();
         project.getUserByUsername(data.consumeString(100));
     }
 }
